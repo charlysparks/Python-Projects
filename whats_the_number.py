e84bea209f26887ce59ee2421d8b6d2c85eb42e9
Charly Sparks <charlotte.spark@gmail.com>
2017-10-12 23:26:41 -0400

    String Change
    Changed the strings of all the outputs so that they are more refined.

--- whats_the_number.py
+++ whats_the_number.py
@@ -8,14 +8,16 @@ print()
 the_number = random.randint(0, 100)
 guess = -1
 
+name = input('Player what is your name? ')
+
 while guess != the_number:
     guess_text = input('Please guess a number between 0 and 100: ')
     guess = int(guess_text)
     if guess < the_number:
-        print('Too low!')
+        print('Sorry {0}, guess of {1} was too LOW.'.format(name, guess))
     elif guess > the_number:
-        print('Too high!')
+        print('Sorry {0}, guess of {1} was too HIGH.'.format(name, guess))
     else:
-        print('YOU WIN!')
+        print('Excellent work {0}, you won! It was {1}.'.format(name,guess))
 
 print('Done.')
